431a1466b715e19fb696f0eb101d78615ba3530b
Mattias Lunderot <matriz@live.se>
2015-03-06 21:32:03 +0100

    Add default constructor and IsObjectWithin

--- BoundingBox.cpp
+++ BoundingBox.cpp
@@ -2,6 +2,12 @@
 
 using namespace DirectX;
 
+BoundingBox::BoundingBox()
+{
+	this->position = XMFLOAT2(0, 0);
+	this->size = XMFLOAT2(0, 0);
+}
+
 BoundingBox::BoundingBox(XMFLOAT2 position, XMFLOAT2 size)
 {
 	this->position = position;
@@ -22,7 +28,9 @@ XMFLOAT2 BoundingBox::GetSize()
 	return size;
 }
 
-bool BoundingBox::IsObjectIntersecting(Object object)
+
+
+bool BoundingBox::IsObjectIntersecting(const Object& object)
 {
 	for (int i = 0; i < object.faces.size(); i++)
 	{
@@ -34,7 +42,19 @@ bool BoundingBox::IsObjectIntersecting(Object object)
 	return false;
 }
 
-bool BoundingBox::IsTriangleIntersecting(Triangle triangle)
+bool BoundingBox::IsObjectWithin(const Object& object)
+{
+	for (int i = 0; i < object.faces.size(); i++)
+	{
+		if (!IsTriangleWithin(object.faces[i]))
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+bool BoundingBox::IsTriangleIntersecting(const Triangle& triangle)
 {
 	for (int i = 0; i < 3; i++)
 	{
@@ -46,9 +66,21 @@ bool BoundingBox::IsTriangleIntersecting(Triangle triangle)
 	return false;
 }
 
-bool BoundingBox::IsPointWithin(XMFLOAT3 point)
+bool BoundingBox::IsTriangleWithin(const Triangle& triangle)
 {
-	//Ignore Y value
+	for (int i = 0; i < 3; i++)
+	{
+		if (!IsPointWithin(triangle.vertices[i]))
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+bool BoundingBox::IsPointWithin(const XMFLOAT3& point)
+{
+	//Ignore Y value - the up and down direction ingame
 	return	position.x < point.x && position.x + size.x > point.x &&
 			position.y < point.z && position.y + size.y > point.z;
 }
--- BoundingBox.h
+++ BoundingBox.h
@@ -10,15 +10,18 @@ private:
 	DirectX::XMFLOAT2 position;
 	DirectX::XMFLOAT2 size;
 public:
+	BoundingBox();
 	BoundingBox(DirectX::XMFLOAT2 position, DirectX::XMFLOAT2 size);
 	~BoundingBox();
 
 	DirectX::XMFLOAT2 GetPosition();
 	DirectX::XMFLOAT2 GetSize();
 
-	bool IsObjectIntersecting(Object object);
-	bool IsTriangleIntersecting(Triangle triangle);
-	bool IsPointWithin(DirectX::XMFLOAT3 point);
+	bool IsObjectIntersecting(const Object& object);
+	bool IsObjectWithin(const Object& object);
+	bool IsTriangleIntersecting(const Triangle& triangle);
+	bool IsTriangleWithin(const Triangle& triangle);
+	bool IsPointWithin(const DirectX::XMFLOAT3& point);
 
 	BoundingBox GetChildBoundingBox(int childQuadrant);
 };
